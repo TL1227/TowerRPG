963bf5a86d43d4c39a6f4be4e1ea59005b70d3e1
Thomas Lavelle <thomas.lavelle@adetiq.co.uk>
2025-07-08 13:32:27 +0100

    Got a square cooking. Really good stuff!

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ using namespace std;
 static string GameVersion = "0.0.0";
 static int SCREEN_WIDTH = 800;
 static int SCREEN_HEIGHT = 600;
+static bool WireFrameMode = false;
 
 void framebuffer_size_callback(GLFWwindow * window, int width, int height)
 {
@@ -20,6 +22,19 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, true);
+    }
+    if (key == GLFW_KEY_F2 && action == GLFW_PRESS)
+    {
+        if (WireFrameMode)
+        {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            WireFrameMode = false;
+        }
+        else
+        {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            WireFrameMode = true;
+        }
     }
 }
 
@@ -69,6 +84,20 @@ void ConsoleSplashMessage()
     cout << "Version" << GameVersion << endl;
 }
 
+void ShowCompileErrors(unsigned int shader)
+{
+    //check for errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
 int main()
 {
     GLFWwindow* window = SetUpGlfw();
@@ -81,14 +110,90 @@ int main()
 
     ConsoleSplashMessage();
 
+	const char* vertSh =
+		"#version 330 core\n"
+        ""
+		"layout(location = 0) in vec3 aPos;"
+		"void main()"
+		"{"
+		    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+		"}";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertSh, NULL);
+    glCompileShader(vertexShader);
+
+    ShowCompileErrors(vertexShader);
+
+    const char* fragSh =
+        "#version 330 core\n"
+        ""
+        "out vec4 FragColor;"
+        "void main()"
+        "{"
+            "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+        "}";
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragSh, NULL);
+    glCompileShader(fragmentShader);
+
+    ShowCompileErrors(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    //TODO: put this linking error check into it's own function
+    char infoLog[512];
+    int success;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //x, y, z
+    vector<float> vertices = {
+        // first triangle
+       0.5f,  0.5f, 0.0f,  // top right
+       0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f,  0.5f, 0.0f,  // top left 
+      // second triangle
+       0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f,  // bottom left
+      -0.5f,  0.5f, 0.0f   // top left
+    };
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int vertBuffer;
+    glGenBuffers(1, &vertBuffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertBuffer);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    glUseProgram(shaderProgram);
 
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-
-        glClear(GL_COLOR_BUFFER_BIT);
     }
 
     glfwTerminate();
